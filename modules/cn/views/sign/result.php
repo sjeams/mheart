64cc536d5070aa05222e410f0a19626f6ee7e146
sjeams <359824901@qq.com>
2022-04-23 10:51:12 +0800

    金服

--- modules/cn/views/sign/result.php
+++ modules/cn/views/sign/result.php
@@ -1,11 +1,11 @@
 <p> 申请编号：<?php echo $res['order_id'] ?> </p> 
 <p> 客户姓名：<?php echo $res['username'] ?> </p> 
 <p> 身份证号： <?php echo $res['card'] ?> </p> 
-<p> 终审额度： <?php echo $res['loan_amount'] ?> 元</p> 
+<p> 终审额度： <?php echo $res['loan_amount'] ? $res['loan_amount'] .'元'  : '<span style="color:red">批款中</span>'?> </p> 
 <p> 利息： 0.45～1.08</p> 
 <p> 还款方式：  等额本息/先息后本 </p> 
-<p> 备注： <?php echo $res['discript'] ?> </p> 
- 
+<p> 备注： <?php echo $res['discript'] ?>  </p> 
+
 <p>紧急联系人员须具备条件如下:</p> 
 <p>1.国企、央企、事业单位、机关单位、公积金个人月缴存600元以上,且连续缴纳满1年,无高额负债</p> 
 <p>2.正式编制职工,且公积金个人月缴存超过1000元以上，且连续缴纳满1年，无高额负债</p> 
