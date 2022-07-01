554d17baa40a02d2468f420da467d8352d37575b
sjeam <359824901@qq.com>
2022-07-01 17:51:32 +0800

    修改

--- cocos/assets/Script/login/myserver.js
+++ cocos/assets/Script/login/myserver.js
@@ -142,7 +142,7 @@ cc.Class({
                 if(data.code==1){// 登录成功，进入游戏
                     // cc.log(data.data.userinfo); 
                     cc.sys.localStorage.setItem('userinfo', JSON.stringify(data.data.userinfo)); 
-                    cc.director.loadScene('/home/dating');
+                    cc.director.loadScene('home/dating');
                     // cc.sys.localStorage.setItem('userinfo', JSON.stringify(data.data.userinfo));
                     // cc.sys.localStorage.getItem(key); //读取数据
                 }
