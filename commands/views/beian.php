ec79673d789629123566abfc66ea6843b243ab34
sjeam <359824901@qq.com>
2022-11-21 17:11:16 +0800

    样式布局

--- commands/views/beian.php
+++ commands/views/beian.php
@@ -1,4 +1,15 @@
-<div class="center">
+<style>
+    .beian{    
+        /* position: fixed; */
+        /* display: flex;
+        margin:  auto;
+        left:0; 
+        right:0; 
+        bottom: 0; */
+    }
+</style>
+
+<div class="center beian">
     <p> Copyright © 2022-现在 心缘测试</p>
     <p> <a href="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_blank" rel="external nofollow">
         蜀ICP备2022010153号-1
