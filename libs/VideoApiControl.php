fe9ab89284341ccf3e0d7a02897cfeb171c44c66
sjeam <359824901@qq.com>
2022-11-25 16:01:24 +0800

    样式

--- libs/VideoApiControl.php
+++ libs/VideoApiControl.php
@@ -25,7 +25,8 @@
                 //token过期userlogin存在保持登录状态，否则销毁
                 $userlogin= Yii::$app->session->get('userlogin');
                 if(!$userlogin){
-                    $userlogin = WechatUser::find()->select('id,name,graden,phone')->where("token='$token'")->asArray()->one();
+                    //修改参数时，注意其它地方也要修改
+                    $userlogin = WechatUser::find()->select('id,name,graden,phone,is_cache')->where("token='$token'")->asArray()->one();
                     // 验证token是否有效--另一设备登录挤下
                     if( $userlogin){
                         Yii::$app->session->set('userlogin',$userlogin);
