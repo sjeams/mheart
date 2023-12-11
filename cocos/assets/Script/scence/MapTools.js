9aef9460bad8f0f4ee1df10d5afee27a7bf0d104
sjeam <359824901@qq.com>
2023-12-11 18:26:48 +0800

    map

--- cocos/assets/Script/scence/MapTools.js
+++ cocos/assets/Script/scence/MapTools.js
@@ -66,7 +66,7 @@ cc.Class({
         this.server_name.getComponent(cc.Label).string=info['name'];
         this.node.getChildByName('server_name').color = new cc.color(type_color);
         //创建一个新button 并将其挂载到创建的精灵下
-        this.bindClickEvent( this.sprite_server_login.getComponent(cc.Button),info_list.map_int);
+        this.bindClickEvent( this.sprite_server_login.getComponent(cc.Button), info);
     },
     // 绑定按钮事件
     bindClickEvent: function (button, index) {
@@ -83,28 +83,25 @@ cc.Class({
  
 
     //按钮点击回调
-    onConfirBtn:function(e,map_int){
+    onConfirBtn:function(e,info){
         console.log(info)
-        cc.sys.localStorage.setItem('figthing', JSON.stringify(map_int)); 
-        // cc.director.loadScene('战斗场景');
-        // var HttpHelper = require("http"); 
-        // var httpRequest = new HttpHelper();
-        // httpRequest.httpPost('/app/app-apiword/in-word', {
-        //     'id': info['id'],
-        //     'star': info['star'],
-        //     'token': null
-        // }, function (data) {
-        //     // 跳转到世界
-        //     // cc.director.loadScene('战斗场景');
-        //     var mask =cc.find("Canvas/mask");
-
-        //     // console.log(data);
-        //     // var server_choes_label  =cc.find("Canvas/server/server_choes/server_choes_label");
-        //     // server_choes_label.getComponent(cc.Label).string=info['name'];
-        //     // var server_choes_type  =cc.find("Canvas/server/server_choes/server_choes_type");
-        //     // server_choes_type.getComponent(cc.Label).string=info['type'];
-        //     // server_choes_type.color = new cc.color(info['color']); 
-        // })
+        var HttpHelper = require("http"); 
+        var httpRequest = new HttpHelper();
+        httpRequest.httpPost('/app/app-apiword/in-word', {
+            'id': info['id'],
+            'star': info['star'],
+            'token': null
+        }, function (data) {
+            //跳转到世界
+            // cc.director.loadScene('战斗场景');
+            cc.sys.localStorage.setItem('figthing', JSON.stringify(info)); 
+            // console.log(data);
+            // var server_choes_label  =cc.find("Canvas/server/server_choes/server_choes_label");
+            // server_choes_label.getComponent(cc.Label).string=info['name'];
+            // var server_choes_type  =cc.find("Canvas/server/server_choes/server_choes_type");
+            // server_choes_type.getComponent(cc.Label).string=info['type'];
+            // server_choes_type.color = new cc.color(info['color']); 
+        })
 
         // var mask =cc.find("Canvas/mask");
         // mask.active=false
