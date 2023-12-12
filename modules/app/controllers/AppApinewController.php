96843775799d0053b92c44e9e8c213cbe6b33464
sjeam <359824901@qq.com>
2023-12-12 14:53:06 +0800

    接口调整

--- modules/app/controllers/AppApinewController.php
+++ modules/app/controllers/AppApinewController.php
@@ -96,7 +96,7 @@ class AppApinewController extends ApiUserControl{
      * http://cs.aheart.com/app/app-apinew/fight
      */
     public function actionFight(){
-        $this->param['map_int']=3;
+        // $this->param['map_int']=3;
         $map_int =$this->param['map_int']?:0;//生物地图序号_阵容编号--系统战斗，获取地图编号时才回用到
         $UserBiologyNatureDo=new UserBiologyNatureDo();
         $UserWords =new UserWords();
