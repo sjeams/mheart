424a528e2e3ecde6859e1a3f69fbfd14b3f6008b
sjeam <359824901@qq.com>
2023-12-19 15:20:29 +0800

    频闭冲突

--- modules/app/models/UserWords.php
+++ modules/app/models/UserWords.php
@@ -149,8 +149,8 @@ class UserWords extends ActiveRecord
         foreach($x_list as $x){
             foreach($y_list as $y){
                 $key++;
-                $map[$key]['x']= 200*($x-1);//0开始
-                $map[$key]['y']= 200*($y-1);//0开始
+                $map[$key]['x']=$left+200*($x-1);//0开始
+                $map[$key]['y']=$height+200*($y-1);//0开始
             }
         }
         
