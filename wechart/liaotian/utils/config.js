489d9fef50ba5588f02b3270bc7872154175c810
sjeam <359824901@qq.com>
2024-01-29 18:34:39 +0800

    swoole通讯

--- wechart/liaotian/utils/config.js
+++ wechart/liaotian/utils/config.js
@@ -1,9 +1,14 @@
+/*
+ * @Author: sjeam
+ * @Date: 2023-10-16 13:38:07
+ * @Description: 
+ */
 module.exports =  {
  
   // 请求域名 格式： https://您的域名
-  HTTP_REQUEST_URL:'http://swoole.mheart.xyz',
+  HTTP_REQUEST_URL:'http://swoole.aheart.cn',
   // Socket链接 暂不做配置
-  WSS_SERVER_URL:'ws://124.221.174.216:9501',
+  WSS_SERVER_URL:'ws://124.222.234.123:9501',
   //JWT token 名称
   TOKEN_NAME:'token',
   //用户注册id 名称
