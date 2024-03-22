8f8c0bc9eae1b27d30e2491238630c337397f445
sjeam <359824901@qq.com>
2024-03-22 11:28:15 +0800

    表单事件

--- web/ckplayer/js/video.js
+++ web/ckplayer/js/video.js
@@ -70,13 +70,12 @@ function  videoList(id,key,isbofang){
     //选择视频
     if(isbofang==1){
     //1 ckplayer 播放器
-        ckplayerVideo(id,now_video,1,now_video_str,url,imageurl)
+        ckplayerVideo(id,now_video,isbofang,now_video_str,url,imageurl)
         // var _this =this;
         // _this.newdplayer.destroy();
     }else{
     //0 dplayer 播放器
-        // dplayerVideo(id,now_video,isbofang,now_video_str,url,imageurl)
-        ckplayerVideo(id,now_video,0,now_video_str,url,imageurl)
+        dplayerVideo(id,now_video,isbofang,now_video_str,url,imageurl)
         // var _this =this;
         // _this.newplayer.remove();
     }
