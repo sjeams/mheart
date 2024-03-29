3a23bddfe7efdd4dbcec0a852f95ee23937d4a7a
sjeam <359824901@qq.com>
2024-03-29 17:47:19 +0800

    样式

--- web/ckplayer/js/ckplayer.js
+++ web/ckplayer/js/ckplayer.js
@@ -448,13 +448,13 @@
 				video.attr('x5-video-orientation','portraint');
 				video.attr('playsinline','true');
 				video.attr('webkit-playsinline','true');
-				video.attr('x5-playsinline','');
+				video.attr('x5-playsinline','true');
 
 
 
 				video.attr('controls','controls');
 				video.attr('x5-video-player-type','h5');
-				video.attr('x5-video-player-fullscreen','');
+				video.attr('x5-video-player-fullscreen','true');
 				if(vars['crossOrigin']){
 					video.useCORS=true;//解决跨域
 		     		video.crossOrigin=vars['crossOrigin'];//解决跨域
