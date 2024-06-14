09c88cc0b6b61876f64cca72797a1c7da0cc006a
sjeam <359824901@qq.com>
2024-06-14 18:37:18 +0800

    背景音乐

--- cocos/assets/Script/场景/AudioManager.js
+++ cocos/assets/Script/场景/AudioManager.js
@@ -1,12 +1,12 @@
 cc.Class({
     extends: cc.Component,
 
-    properties: {
-        bgMusic:{
-            url:cc.AudioClip,
-            default: null
-        },
-    },
+    // properties: {
+    //     bgMusic:{
+    //         url:cc.AudioClip,
+    //         default: null
+    //     },
+    // },
 
     onLoad() {
         cc.game.addPersistRootNode(this.node);
@@ -14,6 +14,9 @@ cc.Class({
 
     
     playBgMusic() {
+
+        var _urls ='/app_resources/loading/武侠.mp3';
+        this.bgMusic = httpRequest.httpUrl(_urls);
        this.bgMusicChannel = cc.audioEngine.play(this.bgMusic,true,0.5)
     },
 
--- cocos/library/imports/35/359c7f12-3452-4f64-acf6-0e27f4e4c9be.js
+++ cocos/library/imports/35/359c7f12-3452-4f64-acf6-0e27f4e4c9be.js
@@ -6,16 +6,18 @@ cc._RF.push(module, '359c78SNFJPZKz2Dif05Mm+', 'AudioManager');
 
 cc.Class({
   "extends": cc.Component,
-  properties: {
-    bgMusic: {
-      url: cc.AudioClip,
-      "default": null
-    }
-  },
+  // properties: {
+  //     bgMusic:{
+  //         url:cc.AudioClip,
+  //         default: null
+  //     },
+  // },
   onLoad: function onLoad() {
     cc.game.addPersistRootNode(this.node);
   },
   playBgMusic: function playBgMusic() {
+    var _urls = '/app_resources/loading/武侠.mp3';
+    this.bgMusic = httpRequest.httpUrl(_urls);
     this.bgMusicChannel = cc.audioEngine.play(this.bgMusic, true, 0.5);
   },
   stopBgMusic: function stopBgMusic() {
