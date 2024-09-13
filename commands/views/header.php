a0bb2ed287317baaba62b65620d569f8a9d7ccb3
sjeam <359824901@qq.com>
2024-09-13 10:49:48 +0800

    清空缓存

--- commands/views/header.php
+++ commands/views/header.php
@@ -74,6 +74,7 @@ $http_index = explode('/', explode('?',$_SERVER["REQUEST_URI"])[0])[3];
         </tr>
     </table>
     <ul class="list-group text-center menu_list">
+        <li class="list-group-item list-item-style btn-defult" onclick="my_chat()"> 聊天</li>
         <?php if($userlogin['graden']>0) {?>
             <li class="list-group-item list-item-style btn-defult" onclick="my_collect()"> 收录</li>
             <li class="list-group-item list-item-style btn-defult" onclick="my_like()"> 极品</li>
@@ -83,7 +84,6 @@ $http_index = explode('/', explode('?',$_SERVER["REQUEST_URI"])[0])[3];
             <!-- <?php   if( explode('?',$_SERVER["REQUEST_URI"])[0]=='/cn/video/list'){ ?><?php }?> -->
             <li class="list-group-item list-item-style  cache_name  <?php $userlogin = Yii::$app->session->get('userlogin'); echo $userlogin['is_cache']?'btn-success':'btn-defult' ?>" onclick="isCache()"> <?php echo $userlogin['is_cache']?'缓存√':'缓存×' ?></li>
         <?php } ?>
-        <li class="list-group-item list-item-style btn-defult" onclick="my_chat()"> 聊天</li>
         <li class="list-group-item list-item-style  bofang_name  <?php $userlogin = Yii::$app->session->get('userlogin'); echo $userlogin['is_bofang']?'btn-success':'btn-defult' ?>" onclick="isBofang()"> <?php echo $userlogin['is_bofang']?'Ckplay':'Dplay' ?></li>
         <li class="list-group-item list-item-style  model_name  <?php $userlogin = Yii::$app->session->get('userlogin'); echo $userlogin['video_model']?'btn-success':'btn-defult' ?>" onclick="vidoeModel()"> <?php echo $userlogin['video_model']?'窗口√':'列表×' ?></li>
         <li class="list-group-item list-item-style btn-defult" onclick="window_reload()">rload</li>
