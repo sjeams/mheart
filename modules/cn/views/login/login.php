54a91561f8317bc29eed4daaf54d5764c5919aba
sjeam <359824901@qq.com>
2025-06-28 17:36:30 +0800

    兼容ios输入法

--- modules/cn/views/login/login.php
+++ modules/cn/views/login/login.php
@@ -70,7 +70,7 @@
             <td>
             <div class="login_check">
                 <span class="logincss">心缘测试</span>
-                <input type="text"  inputmode="text"   name="phone" value="" class="logincss" placeholder="账号：快速注册可用手机号" id="phone">
+                <input type="text"    name="phone" value="" class="logincss" placeholder="账号：快速注册可用手机号" id="phone">
                 <!-- <input type="password"  name="password" value="" class="logincss" placeholder="密码" id="password"> -->
                 <span class="error "></span>
                     <!-- <button onclick="loginIn()" class="logincss">登录</button>             <button onclick="loginIn()" class="logincss">登录</button>  -->
